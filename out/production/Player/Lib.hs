c1683bc319325a606f8296df4afdf2e1bdc21328
Vitaliy <italan13@yandex.ru>
2016-08-07 10:18:32 +0300

    Initial commit

---
+++ out/production/Player/Lib.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+
+module Lib
+    (
+    otherFunc
+    ) where
+
+import Control.Monad (unless)
+import Graphics.UI.Gtk
+import Data.FileEmbed
+import Data.String
+import System.Glib.UTFString
+
+otherFunc :: IO ()
+otherFunc = do
+    let gui = $(embedStringFile "src/resources/player.glade") :: String
+    initGUI
+    -- Create the builder, and load the UI file
+    builder <- builderNew
+    builderAddFromString builder gui
+
+    -- Retrieve some objects from the UI
+    window <- builderGetObject builder castToWindow "window1"
+    button <- builderGetObject builder castToButton "button1"
+
+    -- Basic user interation
+    on button buttonActivated (putStrLn "button pressed!")
+    on window objectDestroy mainQuit
+
+    -- Display the window
+    widgetShowAll window
+    mainGUI
